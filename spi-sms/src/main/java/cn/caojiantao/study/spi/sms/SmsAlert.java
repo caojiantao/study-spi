c29044e647e947f79d22a2a4da80f5620ecb1fe4
caojiantao <13437104137@163.com>
2022-06-22 19:27:46 +0800

    重命名类名

--- spi-sms/src/main/java/cn/caojiantao/study/spi/sms/SmsAlert.java
+++ spi-sms/src/main/java/cn/caojiantao/study/spi/sms/SmsAlert.java
@@ -1,14 +1,14 @@
-package cn.caojiantao.study.spi.wx;
+package cn.caojiantao.study.spi.sms;
 
 import cn.caojiantao.study.spi.alert.IAlert;
 
 /**
  * @author caojiantao
  */
-public class Alert implements IAlert {
+public class SmsAlert implements IAlert {
 
     @Override
     public void warn() {
-        System.out.println("WX");
+        System.out.println("SMS");
     }
 }
--- spi-wx/src/main/java/cn/caojiantao/study/spi/wx/WxAlert.java
+++ spi-wx/src/main/java/cn/caojiantao/study/spi/wx/WxAlert.java
@@ -1,14 +1,14 @@
-package cn.caojiantao.study.spi.sms;
+package cn.caojiantao.study.spi.wx;
 
 import cn.caojiantao.study.spi.alert.IAlert;
 
 /**
  * @author caojiantao
  */
-public class Alert implements IAlert {
+public class WxAlert implements IAlert {
 
     @Override
     public void warn() {
-        System.out.println("SMS");
+        System.out.println("WX");
     }
 }
